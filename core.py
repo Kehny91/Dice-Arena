d6c4c69410114958644e03407a06fc6f46e4fc60
Kehny91 <alexandre@kientz.net>
2024-11-17 14:02:43 +0100

    Reorg

---
+++ core.py
@@ -0,0 +1,270 @@
+from abc import ABC, abstractmethod
+from random import randint
+import numpy as np
+
+
+
+class GameEngine:
+    def __init__(self):
+        self.showPrints = False
+        self.print = self._inactive_print if not self.showPrints else self._active_print
+
+    def _active_print(self, string, end=" "):
+        print(string, end=end)
+
+    def _inactive_print(self, string, end=" "):
+        pass
+
+    def set_show_prints(self, showPrints):
+        self.showPrints = showPrints
+        self.print = self._active_print if showPrints else self._inactive_print
+
+ge = GameEngine()
+
+class GameStat:
+    def __init__(self):
+        self.nbThrows = 0
+
+class Game:
+    def __init__(self):
+        self.entities = []
+
+    def newTurn(self, gameStat= None):
+        ge.print("\nNew Turn. HP left: ",end="")
+        ge.print("|".join([f"{entity.name}: {entity.hp}" for entity in self.entities]))
+
+        for entity in self.entities:
+            entity.playedThisTurn = False
+
+        # Selection alétoire des joueurs
+        # TODO
+        stop = False
+        i = 0
+        while not stop:
+            entityPlaying : Entity = self.entities[i]
+            entityPlaying.resetEffects()
+            while entityPlaying.canPlay(self):
+                rolledFace = entityPlaying.faces[randint(0,len(entityPlaying.faces)-1)]
+                if gameStat is not None:
+                    gameStat.nbThrows += 1
+                target = rolledFace.defaultTarget(self)
+                if target is not None:
+                    ge.print(f"{entityPlaying.name} uses {rolledFace.faceName} on {target.name}, ",end="")
+                else:
+                    ge.print(f"{entityPlaying.name} uses {rolledFace.faceName}, ",end="")
+                rolledFace.apply(self, rolledFace.defaultTarget(self))
+                self.clearDeadGhouls()
+
+            i += 1
+            if i>=len(self.entities):
+                stop = True
+
+    def clearDeadGhouls(self):
+        toClear = []
+        for entity in self.entities:
+            if entity.isGhoul() and (not entity.alive() or not entity.parent.alive()): # Remove ghoul if lich is dead
+                toClear.append(entity)
+        for cl in toClear:
+            self.entities.remove(cl)
+
+    def clearGhouls(self):
+        toClear = []
+        for entity in self.entities:
+            if entity.isGhoul():
+                toClear.append(entity)
+        for cl in toClear:
+            self.entities.remove(cl)
+
+    def canSpawnGhoul(self):
+        ghoulCount = 0
+        maxGhoulCount = 4
+        for entity in self.entities:
+            if entity.isGhoul():
+                ghoulCount += 1
+        return ghoulCount<maxGhoulCount
+
+    def winningTeam(self):
+        """Returns None is there is no winner yet, or the winning team"""
+        inGameTeams = set()
+        for entity in self.entities:
+            if entity.alive():
+                inGameTeams.add(entity.team)
+
+        if len(inGameTeams) == 1:
+            return list(inGameTeams)[0]
+        else:
+            return None
+        
+    def runUntilWinner(self, gameStat = None):
+        turn = 0
+        while self.winningTeam() is None:
+            self.newTurn(gameStat)
+            turn +=1
+            timePerTurn_min = len(self.entities)*10/60 # Chacun prend 10 s pour lancer
+            if turn*timePerTurn_min > 40:
+                print("")
+                for p in self.entities:
+                    p.debug()
+                print("GAME TOO LONG")
+                break
+
+        self.clearGhouls()
+
+                
+
+class Entity:
+    def __init__(self, hp, name, team, parent = None):
+        self.parent = parent
+        self.faces = []
+        self.hp = hp
+        self.name = name
+        self.activeArmor = 0
+        self.immune = False
+        self.concentration = 1
+        self.stunning = None
+        self.playedThisTurn = False
+        self.team = team
+        self.taunting = False
+
+    def alive(self):
+        return self.hp > 0
+    
+    def isGhoul(self):
+        return self.parent is not None
+    
+    def resetEffects(self):
+        """Called before rerolling the dice. DO NOT CALL WHEN REROLLING AFTER CONCENTRATION"""
+        self.activeArmor = 0
+        self.immune = False
+        self.concentration = 1
+        self.stunning = None
+        self.taunting = False
+
+    def canPlay(self, game : Game):
+        for entity in game.entities:
+            if entity.stunning == self:
+                return False
+        return not self.playedThisTurn and self.alive()
+
+    def handleAttack(self, dmg, magic):
+        """Take armor into account. Returns hp lost"""
+        hpLost = 0
+        if not self.immune:
+            if magic:
+                hpLost = dmg
+            else:
+                hpLost = max(0, dmg-self.activeArmor)
+            if self.hp<hpLost:
+                hpLost = self.hp
+            self.hp -= hpLost
+            ge.print(f"{self.name} looses {hpLost} hp")
+        else:
+            ge.print(f"{self.name} is immune")
+        self.hp = max(self.hp, 0)
+        return hpLost
+
+    def handleHeal(self, heal):
+        self.hp += heal
+        ge.print(f"{self.name} heals {heal} hp")
+
+    def isTauntedBy(self, game : Game):
+        """Returns None or the player taunting"""
+        for entity in game.entities:
+            if entity.alive() and entity.team != self.team:
+                if entity.taunting:
+                    return entity
+        return None
+
+    def facesStr(self):
+        return"|".join([f.faceName for f in self.faces])
+
+    def debug(self):
+        print(f"{self.name} : Team {self.team} HP {self.hp} faces : ",end="")
+        print(self.facesStr())
+
+class Face(ABC):
+    def __init__(self, name, owner : Entity, tier):
+        self.faceName = name
+        self.owner = owner
+        self.tier = tier
+    
+    @abstractmethod
+    def defaultTarget(self, game):
+        pass
+
+    @abstractmethod
+    def apply(self, game, target : Entity):
+        """ Apply must set playedThisTurn to True unless it is concentration"""
+        pass
+
+    def _selectWeakestOpp(self, game : Game):
+        """ Prefer not to select ghouls as it is better to kill the Lich"""
+        taunter = self.owner.isTauntedBy(game)
+        if taunter is not None:
+            return taunter
+
+        bestTarget = None
+        bestTargetHealth = None
+
+        for entity in game.entities:
+            if entity.alive() and entity.team != self.owner.team:
+                malus = 20 if entity.isGhoul() else 0
+                if bestTarget is None or entity.hp + malus < bestTarget.hp:
+                    bestTarget = entity
+                    bestTargetHealth = entity.hp
+        
+        return bestTarget
+
+    def _selectWeakestOppWithoutTooMuchArmor(self, game : Game):
+        """Avoid hitting someone under armor. Prefer not to select ghouls as it is better to kill the Lich"""
+        taunter = self.owner.isTauntedBy(game)
+        if taunter is not None:
+            return taunter
+    
+        bestTarget = None
+        bestTargetHealth = None
+
+        for entity in game.entities:
+            if entity.alive() and entity.team != self.owner.team:
+                tooMuchArmor = entity.activeArmor >= self.dmg
+                malus = 20 if entity.isGhoul() else 0
+                if bestTarget is None or (entity.hp + malus < bestTarget.hp and not tooMuchArmor):
+                    # Check if under too much armor
+                    bestTarget = entity
+                    bestTargetHealth = entity.hp
+        
+        return bestTarget
+    
+    def _selectWeakestFriend(self, game : Game):
+        """ Does not select ghoul as they can't be healed """
+        taunter = self.owner.isTauntedBy(game)
+        if taunter is not None:
+            return taunter
+
+        bestTarget = None
+        bestTargetHealth = None
+
+        for entity in game.entities:
+            if entity.alive() and entity.team == self.owner.team and not entity.isGhoul():
+                if bestTarget is None or entity.hp < bestTarget.hp:
+                    bestTarget = entity
+                    bestTargetHealth = entity.hp
+        
+        return bestTarget
+    
+    def _selectNone(self, game : Game):
+        return None
+    
+    def _selectSelf(self, game : Game):
+        return self.owner
+    
+def getNIndexesRandomly(elements, N, mustBeDifferent):
+    """elements is passed but really, we just need its length"""
+    pickedIndexes = []
+    allIndexes = list(range(len(elements)))
+    for k in range(N):
+        if mustBeDifferent:
+            pickedIndexes.append(allIndexes.pop(randint(0,len(allIndexes)-1)))
+        else:
+            pickedIndexes.append(allIndexes[randint(0,len(allIndexes)-1)])
+    return pickedIndexes
---
+++ faces.py
@@ -0,0 +1,269 @@
+from core import Face,Entity,Game,getNIndexesRandomly,ge
+
+
+class Fail(Face):
+    def __init__(self, owner : Entity):
+        super().__init__("FAIL", owner, 0)
+
+    def apply(self, game, target : Entity):
+        ge.print("nothing happens")
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectNone(game)
+
+class Attack(Face):
+    def __init__(self, owner : Entity, dmg, tier):
+        super().__init__("Attack"+str(dmg), owner, tier)
+        self.dmg = dmg
+
+    def apply(self, game, target : Entity):
+        """target must be the one to attack"""
+        if target is None:
+            ge.print("No one to attack")
+        else:
+            target.handleAttack(self.owner.concentration*self.dmg,False)
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectWeakestOppWithoutTooMuchArmor(game)
+
+class Heal(Face):
+    def __init__(self, owner : Entity, heal, tier):
+        super().__init__("Heal"+str(heal), owner, tier)
+        self.heal = heal
+
+    def apply(self, game, target : Entity):
+        """target must be the one to heal"""
+        # Target can't be None as the caster is alive
+        target.handleHeal(self.owner.concentration*self.heal)
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectWeakestFriend(game)
+
+class Armor(Face):
+    def __init__(self, owner : Entity, armor, tier):
+        super().__init__("Armor"+str(armor), owner, tier)
+        self.armor = armor
+
+    def apply(self, game, target: Entity):
+        """target must be the one to armor"""
+        # Target can't be None as the caster is alive
+        target.activeArmor = self.owner.concentration*self.armor
+        self.owner.playedThisTurn = True
+        
+        ge.print(f"{target.name} gains {self.owner.concentration*self.armor} armor")
+
+    def defaultTarget(self, game):
+        return self._selectSelf(game)
+
+class Concentration(Face):
+    def __init__(self, owner : Entity, tier):
+        super().__init__("Concentration", owner, tier)
+
+    def apply(self, game, target: Entity):
+        """target is ignored"""
+        self.owner.concentration *= 2
+        ge.print(f"{self.owner.name} concentrates")
+        assert target is None, "WEIRD"
+        # Do not set self.owner.playedThisTurn to True
+            
+    def defaultTarget(self, game):
+        return self._selectNone(game)
+
+class Stun(Face):
+    def __init__(self, owner : Entity, tier):
+        super().__init__("Stun", owner, tier)
+    
+    def apply(self, game, target: Entity):
+        """Target is stunned"""
+        if target is None:
+            ge.print("No one to stun")
+        else:
+            self.owner.stunning = target
+            ge.print(f"{target.name} is stunned")
+        self.owner.playedThisTurn = True
+        
+
+    def defaultTarget(self, game):
+        return self._selectWeakestOpp(game)
+
+class Sweep(Face):
+    def __init__(self, owner: Entity, dmg, tier):
+        super().__init__("Sweep"+str(dmg), owner, tier)
+        self.dmg = dmg
+
+    def apply(self, game, target: Entity):
+        """Target is ignored"""
+        for entity in game.entities:
+            if entity.team != self.owner.team and entity.alive():
+                entity.handleAttack(self.owner.concentration*self.dmg,False)
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectNone(game)
+
+class Fireball(Face):
+    def __init__(self, owner : Entity, dmg, tier):
+        super().__init__("Fireball"+str(dmg), owner, tier)
+        self.dmg = dmg
+
+    def apply(self, game, target : Entity):
+        """target must be the one to attack"""
+        if target is None:
+            ge.print("No one to fireball")
+        else:
+            target.handleAttack(self.owner.concentration*self.dmg,True)
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectWeakestOpp(game)
+
+class Upgrade(Face):
+    """Concentration is ignored"""
+    def __init__(self, owner : Entity, tier1Stack, tier2Stack, tier3Stack):
+        super().__init__("Upgrade", owner)
+        self.tierStack = [tier1Stack,tier2Stack,tier3Stack]
+
+    def apply(self, game, target: Entity):
+        """Target is ignored. always upgrade the weakest face"""
+        weakestFaceIndex = None
+        weakestFaceTier = None
+        for k,v in enumerate(self.owner.faces):
+            if weakestFaceTier is None or v.tier < weakestFaceTier:
+                weakestFaceIndex = k
+                weakestFaceTier = v.tier
+
+        index  = getNIndexesRandomly(self.tierStack[max(weakestFaceTier+1,3)],1,False)[0]
+        self.owner.faces.pop(weakestFaceIndex)
+        self.owner.faces.insert(weakestFaceIndex,self.tierStack[max(weakestFaceTier+1,3)][index])
+
+        def defaultTarget(self, game):
+            return self._selectSelf(game)
+
+class Tank(Face):
+    def __init__(self, owner : Entity):
+        super().__init__("Tank", owner, 4)
+        self.armor = 4
+
+    def apply(self, game, target: Entity):
+        """Target is ignored"""
+        self.owner.taunting = True
+        self.owner.activeArmor = self.owner.concentration*self.armor
+        ge.print(f"{target.name} gains {self.owner.concentration*self.armor} armor")
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectSelf(game)
+
+class Vampire(Face):
+    def __init__(self, owner):
+        super().__init__("Vampire", owner, 4)
+
+    def apply(self, game, target):
+        hpLost = 0
+        if target is None:
+            ge.print("No one to Vampireise")
+        else:
+            hpLost = target.handleAttack(2*self.owner.concentration,True)
+        self.owner.handleHeal(hpLost)
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectWeakestOpp(game)
+
+class King(Face):
+    def __init__(self, owner):
+        super().__init__("King", owner, 4)
+        self.dmg = 2 # Needed for _selectWeakestOppWithoutTooMuchArmor
+        self.heal = 1
+        self.armor = 1
+
+    def apply(self, game, target):
+        if target is None:
+            ge.print("No one to attack")
+        else:
+            target.handleAttack(self.dmg*self.owner.concentration,False)
+        self.owner.handleHeal(self.heal*self.owner.concentration)
+        self.owner.activeArmor = self.armor*self.owner.concentration
+        ge.print(f"{self.owner.name} gains {self.owner.concentration*self.armor} armor")
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectWeakestOppWithoutTooMuchArmor(game)
+
+
+class Paladin(Face):
+    def __init__(self, owner):
+        super().__init__("Paladin", owner, 4)
+
+    def apply(self, game, target):
+        self.owner.immune = True
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectSelf(game)
+
+class Lich(Face):
+    def __init__(self, owner):
+        super().__init__("Lich", owner, 4)
+
+    def apply(self, game : Game, target):
+        """target is ignored"""
+        nbOfGhoulsToSpawn = 1*self.owner.concentration
+        for k in range(nbOfGhoulsToSpawn):
+            if game.canSpawnGhoul():
+                ghoul = createGhoul(self.owner)
+                ghoul.playedThisTurn = True # Ghoul cannot play immediatly
+                ge.print("ghoul spawned")
+                game.entities.append(ghoul)
+            else:
+                ge.print("too many ghouls")
+        self.owner.playedThisTurn = True
+
+    def defaultTarget(self, game):
+        return self._selectSelf(game)
+    
+def createGhoul(father : Entity):
+    ghoulHp = 1
+    p = Entity(ghoulHp, "Ghoul", father.team, father)
+    p.faces.append(Fail(p))
+    p.faces.append(Fail(p))
+    p.faces.append(Fail(p))
+    p.faces.append(Fail(p))
+    p.faces.append(Attack(p,2,1))
+    p.faces.append(Attack(p,1,1))
+    return p
+
+def addSpellByString(player, string, tier):
+    if string[0:3] == "Att":
+        player.faces.append(Attack(player,int(string[-1]), tier))
+    elif string[0:3] == "Hea":
+        player.faces.append(Heal(player,int(string[-1]), tier))
+    elif string[0:3] == "Swe":
+        player.faces.append(Sweep(player,int(string[-1]), tier))
+    elif string[0:3] == "Fir":
+        player.faces.append(Fireball(player,int(string[-1]), tier))
+    elif string[0:3] == "Arm":
+        player.faces.append(Armor(player,int(string[-1]), tier))
+    elif string[0:3] == "Con":
+        player.faces.append(Concentration(player, tier))
+    elif string[0:3] == "Tan":
+        player.faces.append(Tank(player))
+    elif string[0:3] == "Vam":
+        player.faces.append(Vampire(player))
+    elif string[0:3] == "Kin":
+        player.faces.append(King(player))
+    elif string[0:3] == "Pal":
+        player.faces.append(Paladin(player))
+    elif string[0:3] == "Lic":
+        player.faces.append(Lich(player))
+    elif string[0:3] == "Fai":
+        player.faces.append(Fail(player))
+    else:
+        assert False, "WEIRD"
+
+def addAllSpellsByString(player, spells, tier):
+    for spell in spells:
+        addSpellByString(player, spell, tier)
